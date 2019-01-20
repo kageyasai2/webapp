0ca7791f5d115ea7b2c99f80ed9f081b26ac7b64
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-01-20 15:02:41 +0900

    [add]コンテナ間で接続できるように設定 & DB

--- web_go/app/main.go
+++ web_go/app/main.go
@@ -1,15 +1,50 @@
 package main
 
-import(
+import (
+    "database/sql"
     "fmt"
-    "net/http"
+
+    _ "github.com/lib/pq"
 )
 
-func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":5000", nil)
+type EMPLOYEE struct {
+    ID     string
+    NUMBER string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, from Docker container!")
+func main() {
+    fmt.Println("aaaa")
+    db, err := sql.Open("postgres", "host=172.23.0.1 port=5123 user=root password=password dbname=testdb sslmode=disable")
+    defer db.Close()
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    // INSERT
+    var empID string
+    id := 3
+    number := 4444
+    err = db.QueryRow("INSERT INTO employee(emp_id, emp_number) VALUES($1,$2) RETURNING emp_id", id, number).Scan(&empID)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    fmt.Println(empID)
+
+    // SELECT
+    rows, err := db.Query("SELECT * FROM employee")
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    var es []EMPLOYEE
+    for rows.Next() {
+        var e EMPLOYEE
+        rows.Scan(&e.ID, &e.NUMBER)
+        es = append(es, e)
+    }
+    fmt.Printf("%v", es)
 }
